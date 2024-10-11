abc0730f7a20160541d66021b9567b7785bc9a20
AlexIvankovich <ivankovichalmih@gmail.com>
2024-10-11 11:41:34 +0300

    Delete src/test/java/FirstTest.java

--- src/test/java/FirstTest.java
+++
@@ -1,9 +0,0 @@
-import org.testng.annotations.Test;
-
-public class FirstTest extends BaseTest {
-
-    @Test
-    public void firsttest() {
-        driver.get("https://www.onliner.by/");
-    }
-}
